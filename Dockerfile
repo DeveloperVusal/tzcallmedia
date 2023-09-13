027055e96e2f9ff45f2cefb98151bd6740a94946
DeveloperVusal <developer.vusal96@gmail.com>
2023-09-13 14:39:11 +0300

    feat: add clickhouse and docker compose

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM php:8.2-fpm-alpine
+
+FROM composer:2.0 as vendor
+
+# Install Composer
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+RUN composer install
--- Executers/Queue.php
+++ Executers/Queue.php
@@ -2,15 +2,18 @@
 namespace Executers;
 
 use Connections\Conn;
+use Connections\Drivers\ClickHouse;
 use Connections\Drivers\MariaDB;
 
 class Queue {
 
     private MariaDB $mariadb;
+    private ClickHouse $clhouse;
     
     function __construct()
     {
-        $this->mariadb = Conn::driver('\\Connections\\Drivers\\MariaDB', 'localhost', 3306, 'root', 'root', 'callmedia');
+        $this->mariadb = Conn::driver(get_class($this->mariadb), 'localhost', 3306, 'root', 'root', 'callmedia');
+        $this->clhouse = Conn::driver(get_class($this->clhouse), 'localhost', 9004, 'default ', '', 'callmedia');
     }
 
     public function setDb(string $url): void
@@ -20,6 +23,11 @@ class Queue {
         $sth = $this->mariadb->dbn()->prepare("INSERT INTO urls_data (url, length) VALUES(?, ?)");
         $isset = $sth->execute([$curl->info()['url'], $curl->getContentLength()]);
 
-        echo ($isset)?"  [+] Set is successfully \n":"  [x] Set is not successfully \n";
+        echo ($isset)?"  [+] Set is successfully in MySQL \n":"  [x] Set is not successfully in MySQL\n";
+
+        $sth2 = $this->clhouse->dbn()->prepare("INSERT INTO urls_data (url, length) VALUES(?, ?)");
+        $isset2 = $sth2->execute([$curl->info()['url'], $curl->getContentLength()]);
+
+        echo ($isset2)?"  [+] Set is successfully in ClickHouse \n":"  [x] Set is not successfully in ClickHouse\n";
     }
 }
